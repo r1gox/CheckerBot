db9ca0de8b129accd8c80148e3b7b5fff7f71f25
Rigo Jiménez <70866911+r1gox@users.noreply.github.com>
2024-08-03 19:44:35 -0600

    Update webhook.php

--- CheckerBot/public/webhook.php
+++ CheckerBot/public/webhook.php
@@ -13,7 +13,6 @@ header("Pragma: no-cache");
 $token = "5405339405:AAG0kGkeN-8VueVsI2JCLQbHI3wYSnfoG7Y";
 $website = "https://api.telegram.org/bot".$token;
 
-
 $data = file_get_contents("php://input");
 $json = json_decode($data, true);
 $update = $json["message"];
@@ -416,26 +415,4 @@ function sendMessage($chatID, $respuesta) {
         $url = $GLOBALS["website"]."/sendMessage?chat_id=".$chatID."&parse_mode=HTML&text=".urlencode($respuesta);
         file_get_contents($url);
 }
-
-/*
-//FUNCTION SEND
-function sendMessage($chatID, $messaggio, $token,&$k = ''){
-    $url = "https://api.telegram.org/bot".$token."/sendMessage?disable_web_page_preview=false&parse_mode=HTML&chat_id=".$chatID;
-
-
-//        if(isset($k)) {
-  //              $url = $url."&reply_markup=".$k;
-  //              }
-
-
-    $url = $url."&text=" . urlencode($messaggio);
-    $ch = curl_init();
-    $optArray = array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true
-    );
-    curl_setopt_array($ch, $optArray);
-    $result = curl_exec($ch);
-    curl_close($ch);
-}*/
 ?>
