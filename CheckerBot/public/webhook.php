203e5afb14e8ab07ec2b774b2dfb6e9074bdced7
Rigo Jiménez <70866911+r1gox@users.noreply.github.com>
2024-12-26 22:08:05 -0600

    Update webhook.php

--- CheckerBot/public/webhook.php
+++ CheckerBot/public/webhook.php
@@ -466,8 +466,8 @@ if ($update["from"]["id"] == $myid || in_array($update["from"]["id"], $autorizad
 
 if((strpos($message, "!") === 0)||(strpos($message, "/") === 0)||(strpos($message, ".") === 0)){
 
-$timeout = 60; // Tiempo de espera en segundos
-$maxMessages = 2; // Máximo de mensajes permitidos
+$timeout = 40; // Tiempo de espera en segundos
+$maxMessages = 3; // Máximo de mensajes permitidos
 $file = 'users.txt';
 $userId = $id; // Reemplaza con la lógica para obtener el ID del usuario actual
 
