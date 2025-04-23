0f5508f2dd86570d00d6e05252603aec335f8509
Rigo Jiménez <70866911+r1gox@users.noreply.github.com>
2025-04-23 13:27:51 -0600

    Update config.php

--- CheckerBot/public/config.php
+++ CheckerBot/public/config.php
@@ -2,7 +2,7 @@
 
 //$token = "5405339405:AAG0kGkeN-8VueVsI2JCLQbHI3wYSnfoG7Y";
 //$token = "5328115503:AAHl8GmE0Wfi1EbF-wtr4jwMPl-jH2TQ2lE";
-$token = "5787106045:AAF2rgsLGfR8bXH1jeSUYYr7O6-Y2n3-gvs";
+//$token = "5787106045:AAF2rgsLGfR8bXH1jeSUYYr7O6-Y2n3-gvs";
 $config['sk_keys'] =  array('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');
 //$config['sk_keys'] =  array('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');
 
